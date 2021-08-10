363cc3a74be4b337778cf20fa0f172c9c0147982
Zbigniew Bernacki <zbernacki@aiut.com.pl>
2021-08-10 11:07:58 +0200

    dynamic datas

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
+
+    const { transactions } = useContext(GlobalContext)
+
+    const amounts = transactions.map(el => el.amount)
+
+    const totalBalance = amounts.reduce((prev, next) => (prev += next), 0).toFixed(2);
     return (
         <>
             <h4>Your Balance</h4>
-            <h1 >$0.00</h1>
+            <h1 >${totalBalance}</h1>
         </>
     )
 }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,7 +5,7 @@ export const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+'
 
     return (
-        <li className='minus'>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text}
             <span>{sign}${Math.abs(transaction.amount)}</span> {/* Math.abs daje zawsze pozytywne liczby poniewaz bez tej funkcji bylby podwójny minus przed wartoscia */}
             <button>x</button>
